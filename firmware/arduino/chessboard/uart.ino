31b90b7b82ee18ee5a6925d522b0d48a005b0e3f
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-11-09 13:51:39 -0500

    Created UART input logic and placeholder movement function declarations

---
+++ firmware/arduino/chessboard/uart.ino
@@ -0,0 +1,62 @@
+char incomingByte;
+char  buffer[5];
+int byteNum = 0;
+
+//Wait for input
+void serialEvent2()
+{
+    //Loop through all available bytes
+    while (Serial2.available())
+    {
+        //Get byte
+        incomingByte = (char) Serial2.read();
+
+        //Reset buffer position
+        if (incomingByte == '~')
+        {
+            byteNum = 0;
+        }
+        //Add byte to buffer
+        else
+        {
+            buffer[byteNum] = incomingByte;
+        }
+
+        //Check if the buffer is full, process input
+        if (byteNum == 4)
+        {
+            //Reset buffer position
+            byteNum = 0;
+
+            //Process input
+            switch(buffer[0])
+            {
+                case 0:
+                    //move_straight(int startRow, int startCol, int endRow, int endCol)
+                    move_straight(buffer[1] - 'a', buffer[2] - '0', buffer[3] - 'a', buffer[4] - '0');
+                    break;
+                case 1:
+                    //move_to_graveyard(int startRow, int startCol, char color, char type)
+                    move_to_graveyard(buffer[1] - 'a', buffer[2] - '0', buffer[3], buffer[4]);
+                    break;
+                case 2:
+                    //Args: piece color (b/w), kingside or queenside (-/0) 
+                    //castle(char color, char side)
+                    castle(buffer[1], buffer[3]);
+                    break;
+                case 3:
+                    //move_along_edges(int startRow, int startCol, int endRow, int endCol)
+                    move_along_edges(buffer[1] - 'a', buffer[2] - '0', buffer[3] - 'a', buffer[4] - '0');
+                    break;
+                default:
+                    //Handle error. Idk how we plan on doing this yet.
+                    break;
+            }
+        }
+        //Increment buffer position
+        else
+        {
+            byteNum++;
+        }
+    }
+}
