276e21d85b7aa9368dff9d68abd7b1e36efcbcc4
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-11-14 18:27:40 -0500

    Added movement functions

---
+++ firmware/arduino/chessboard/movement_functions.ino
@@ -0,0 +1,19 @@
+void move_straight(int startRow, int startCol, int endRow, int endCol)
+{
+    return;
+}
+
+void move_to_graveyard(int startRow, int startCol, char color, char type)
+{
+    return;
+}
+
+void castle(char color, char queenside_or_kingside)
+{
+    return;
+}
+
+void move_along_edges(int startRow, int startCol, int endRow, int endCol)
+{
+    return;
+}
