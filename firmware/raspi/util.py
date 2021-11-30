a9d62361abf70de3695519f88b36361dc652ef01
Nashir Janmohamed <nashirbass@gmail.com>
2021-11-30 16:45:01 -0500

    Update util.py for style

--- firmware/raspi/util.py
+++ firmware/raspi/util.py
@@ -1,11 +1,22 @@
-class BoardCell:
-	def __init__(self, x=None, y=None):
-		''' Point, if no args passed, initialized to (0,0), top left corner of board
-		
-		Corresponds to center of board square
-		'''
-		self.x = x if x else 0
-		self.y = y if y else 0
+'''Helper file for miscellaneous utility classes and functions.
+'''
 
-	def __str__(self):
-		return f"{chr(self.x+ord('a'))}{chr(self.y+ord('a'))}"
+class BoardCell:
+    '''Helper class for indexing entirety of board.
+    '''
+    def __init__(self, row=None, col=None):
+        '''Point, if no args passed, initialized to (0,0), top left corner of board
+
+        Corresponds to center of board square
+        '''
+        self.row = row if row else 0
+        self.col = col if col else 0
+
+    def __str__(self):
+        # TODO: Update this to have a clearer naming scheme for the indexing.
+        return f"{chr(self.row+ord('a'))}{chr(self.col+ord('a'))}"
+
+    def get_coords(self):
+        '''Returns tuple of row and col indices.
+        '''
+        return (self.row, self.col)
