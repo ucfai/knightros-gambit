0100694a1daea5c337036bd85dc8957c482a1e4e
JuddB <63137249+JuddBE@users.noreply.github.com>
2021-11-29 19:37:32 -0500

    Update NN Layout.py

--- chess-AI/experimental-work/NN Layout.py
+++ chess-AI/experimental-work/NN Layout.py
@@ -13,9 +13,9 @@ import torchvision
 
 class PlayNetwork(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(PlayNetwork, self).__init__()
         
-        # Basic layout for AI CNN, still need to fix BatchNorm2d input sizes and double check value head.
+        # Basic layout for AI CNN, still need to polish the value head and BatchNorm2d input sizes.
         
         # Reference: Alpha Zero Paper pg. 13-14
         # Input conv layer takes (MT + L) channels of input, each channel with N * N dimensions.
@@ -23,23 +23,25 @@ class PlayNetwork(nn.Module):
         # M = 14 representing the 12 channels of the position of each piece type and 2 channels for repetition,
         # T = 8 representing the number of past moves to consider,
         # L = 7 representing the number of constant number planes besides repetition such as castling, move count, etc.
-        self.conv1 = nn.Conv2d(in_channels=119, out_channels=256, kernel_size=3, padding=1)
-        self.bn = nn.BatchNorm2d(21)
+        self.convLayer = nn.Sequential(nn.Conv2d(in_channels=19, out_channels=256, kernel_size=3, padding=1),
+                                       nn.BatchNorm2d(256),
+                                       nn.ReLU(inplace=True))
         
         # Each conv layer in a residual block uses 256 3x3 filters, padding used to keep the channel dimensions constant.
         self.resBlock = nn.Sequential(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1), 
-                                    nn.BatchNorm2d(21), 
+                                    nn.BatchNorm2d(256), 
                                     nn.ReLU(inplace=True), 
                                     nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1), 
-                                    nn.BatchNorm2d(21))
+                                    nn.BatchNorm2d(256))
         
         # Use 2 1x1 filters to convolve input channels to 2 output channels, one representing piece
         # to move, and the other representing move to take out from possible moves.
-        # Use them to pick move from possible moves, represented by 73 channels of 8x8, or 4672 possible moves.
+        # Use them to pick move from possible moves, represented by 73 channels of 8x8, or 4672 possible moves <- (Revising)
         self.policyHead = nn.Sequential(nn.Conv2d(in_channels=256, out_channels=2, kernel_size=1),
                                        nn.BatchNorm2d(2),
                                        nn.ReLU(inplace=True),
-                                       nn.Linear(in_features=2, out_features=73))
+                                       nn.Flatten(),
+                                       nn.Linear(in_features=2*8*8, out_features=73))
         
         # Convolve 256 8x8 channels into 8x8 channel, then use fully connected layer to take 64 input features from 8x8
         # channel and transform to 256 output features, then transform to one scalar value.
@@ -47,7 +49,7 @@ class PlayNetwork(nn.Module):
                                        nn.BatchNorm2d(1),
                                        nn.ReLU(inplace=True),
                                        nn.Flatten(),
-                                       nn.Linear(in_features=64, out_features=256),
+                                       nn.Linear(in_features=8*8, out_features=256),
                                        nn.ReLU(inplace=True),
                                        nn.Linear(in_features=256, out_features=1),
                                        nn.Tanh())
@@ -55,22 +57,27 @@ class PlayNetwork(nn.Module):
         
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = self.convLayer(x)
         
         # Go through all the residual blocks and add the input of the block to the output before regularization.
-        for numRes in range(1):
+        for numRes in range(3):
             shortcut = x
-            x = resBlock(x)
-            x = nn.ReLU(x + shortcut)
+            x = self.resBlock(x)
+            x = F.relu_(x + shortcut)
         
         value_out = self.valueHead(x)
         policy_out = self.policyHead(x)
         
         return (policy_out, value_out)
     
-
-# model = PlayNetwork()
-# policy, value = model()
+    
+    
+model = PlayNetwork()
+policy, value = model(torch.randn(1, 19, 8, 8))
+policy = policy.reshape(73)
+value = value.item()
+print(policy)
+print(value)
 
 
 
