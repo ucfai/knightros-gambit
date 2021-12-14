9f6ba6a851ac3b148821491812f046df80fe691f
Aden McKinney <aden.mckinney10@gmail.com>
2021-12-14 14:34:22 -0500

    checking out

--- firmware/arduino/chessboard/motorDriver.ino
+++ firmware/arduino/chessboard/motorDriver.ino
@@ -27,39 +27,28 @@ void moveMotor(int motor[], int steps, int dir)
 
 void moveSpaces(int motor[], int spaces, int dir)
 {
-  int i;
-
   // How many steps per space
   float numSteps = spaces * stepsPerSpace;
 
-  digitalWrite(MOTOR_SLEEP, HIGH);
-  digitalWrite(MOTOR_RESET, HIGH);
-  digitalWrite(MOTOR_ENABLE, LOW);
-
   digitalWrite(motor[1], dir);
 
-  for(i = 0; i < numSteps; i++);
-  {
-    digitalWrite(motor[0], LOW);
-    delay(1);  // 1 milliSecond
-    digitalWrite(motor[0], HIGH);
-  }
-
+  moveMotor(motor[], (int)numSteps, dir)
 }
 
-void moveDiagonal(int motor[], int dir, int spaces)
+void moveDiagonal(int dirX, int dirY, int spacesX, int spacesY)
 {
   int i;
 
   // similary subject to change as above function dictation, same number as above though
-  float numSteps = spaces * stepsPerSpace;
+  float numStepsX = spacesX * stepsPerSpace;
+  float numStepsX = spacesY * stepsPerSpace;
 
   digitalWrite(MOTOR_SLEEP, HIGH);
   digitalWrite(MOTOR_RESET, HIGH);
   digitalWrite(MOTOR_ENABLE, LOW);
 
-  digitalWrite(xMotor[1], dir);
-  digitalWrite(yMotor[1], dir);
+  digitalWrite(xMotor[1], dirX);
+  digitalWrite(yMotor[1], dirX);
 
   for(i = 0; i < numSteps; i++)
   {
