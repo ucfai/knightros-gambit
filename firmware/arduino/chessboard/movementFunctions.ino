d7bb097033d9b4a6e36e0e0dd73ad05ec5a2f049
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-12-22 22:54:40 -0500

    Removed unnecessary movement functions

--- firmware/arduino/chessboard/movementFunctions.ino
+++ firmware/arduino/chessboard/movementFunctions.ino
@@ -3,16 +3,6 @@ void moveDirect(int startRow, int startCol, int endRow, int endCol)
     return;
 }
 
-void moveToGraveyard(int startRow, int startCol, char color, char type)
-{
-    return;
-}
-
-void castle(char color, char queenside_or_kingside)
-{
-    return;
-}
-
 void moveAlongEdges(int startRow, int startCol, int endRow, int endCol)
 {
     return;
