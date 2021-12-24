3b0edc1eb743e903f435c598c899326e0d6c5922
Nashir Janmohamed <nashirbass@gmail.com>
2021-12-23 21:38:38 -0800

    Changes status codes to chars instead of int for readability

--- firmware/raspi/status.py
+++ firmware/raspi/status.py
@@ -3,10 +3,10 @@ class ArduinoStatus:
     # TODO: add in some more documentation here w.r.t. message formatting.
     '''
     # Status codes used to indicate current status of Arduino controlling physical board.
-    IDLE = 0
-    EXECUTING_MOVE = 1
-    END_TURN_BUTTON_PRESSED = 2
-    ERROR = 3
+    IDLE = '0'
+    EXECUTING_MOVE = '1'
+    END_TURN_BUTTON_PRESSED = '2'
+    ERROR = '3'
 
     def __init__(self, status, move_count, extra):
         self.status = status
@@ -45,22 +45,24 @@ class OpCode:
     '''Enum of op codes used to provide information about type of move Arduino should make.
     '''
     # This code indicates Arduino should use straight-line path, diagonals allowed
-    MOVE_PIECE_IN_STRAIGHT_LINE = 0
+    MOVE_PIECE_IN_STRAIGHT_LINE = '0'
 
     # This code indicates Arduino should move piece along square edges instead of
     # through the center of squares. Used for knights if adjacent pieces, for
     # graveyard pathing, and for castling
-    MOVE_PIECE_ALONG_SQUARE_EDGES = 1
+    MOVE_PIECE_ALONG_SQUARE_EDGES = '1'
 
     # Queenside and kingside castling are composed of two moves; one `MOVE_PIECE_IN_STRAIGHT_LINE`
     # (king moving in straight line) and one `MOVE_PIECE_ALONG_SQUARE_EDGES`
 
     # This code indicates Arduino should use electromagnet to center piece on square
-    ALIGN_PIECE_ON_SQUARE = 2
+    ALIGN_PIECE_ON_SQUARE = '2'
 
 class ArduinoException(Exception):
     '''Helper class for custom Arduino exceptions.
     '''
-    NONE = 0
-    INVALID_OP = 1
-    INVALID_LOCATION = 2
+    NO_ERROR = '0'
+    INVALID_OP = '1'
+    INVALID_LOCATION = '2'
+    INCOMPLETE_INSTRUCTION = '3'
+    MOVEMENT_ERROR = '4'
