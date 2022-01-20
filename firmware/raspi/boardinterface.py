0df67966e838586c7a19936a970598d1d31cb26c
Nashir Janmohamed <nashirbass@gmail.com>
2022-01-19 19:10:50 -0500

    Fix calls to util.get_2d_board

--- firmware/raspi/boardinterface.py
+++ firmware/raspi/boardinterface.py
@@ -121,7 +121,7 @@ class Engine:
     def get_piece_info_from_square(self, square):
         '''Returns tuple of color and piece type from provided square.
         '''
-        return util.get_piece_info_from_square(square, self.get_2d_board())
+        return util.get_piece_info_from_square(square, util.get_2d_board(self.fen()))
 
     def outcome(self):
         '''Returns None if game in progress, otherwise outcome of game.
@@ -254,7 +254,7 @@ class Board:
         '''Prints board as 2d grid.
         '''
         # (0, 0) corresponds to a1, want to print s.t. a1 is bottom left, so reverse rows
-        chess_grid = util.get_2d_board(self.engine.chess_board.fen())
+        chess_grid = util.get_2d_board(self.engine.fen())
         chess_grid.reverse()
         # 8 x 8 chess board
         for i in range(8):
@@ -334,7 +334,7 @@ class Board:
 
         source, dest = Engine.get_chess_coords_from_uci_move(uci_move)
 
-        board_2d = util.get_2d_board(self.engine.chess_board.fen())
+        board_2d = util.get_2d_board(self.engine.fen())
         # Cut number of cases from 8 to 4 by treating soure and dest interchangeably
         left, right = (source, dest) if source.col < dest.col else (dest, source)
         if left.col == right.col - 1:
