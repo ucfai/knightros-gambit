7d98d0b3c4c04e873f8945cddbd4d554701b7080
fshcat <jhoyos314@gmail.com>
2022-01-21 15:09:13 -0500

    Refactored training, implement multigame datasets

--- chess-AI/train.py
+++ chess-AI/train.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import random
 import time
 
@@ -24,141 +25,135 @@ class MctsTrain:
     def __init__(self, mcts_simulations, exploration, lr):
         # Set the number of Monte Carlo Simulations
         self.mcts_simulations = mcts_simulations
-
-        # Stores mcts probabilities and outcome reward values
-        self.mcts_probs = []
-        self.mcts_evals = []
-
-        # Stores board fen strings
-        self.board_fens = []
-
         self.mcts = Mcts(exploration)
+
         self.policy_converter = PlayNetworkPolicyConverter()
 
         # Learning rate
         self.lr = lr
 
-    def training_episode(self, nnet, epochs):
-        with torch.no_grad():
-            # At each episode need to set the training example and network predictions to empty
-            self.mcts_probs = []
-            self.mcts_evals = []
-            self.board_fens = []
+    def training_game(self, nnet):
+        # Stores final probability distribution from MCTS
+        mcts_probs = []
 
-            batch_size = 8
-            num = 0
+        # Store fen_strings for board states
+        board_fens = []
 
-            for _ in range(epochs):
-                # The board needs to be initialized to the starting state
-                board = chess.Board()
+        board = chess.Board()
+        while True:
+            fen_string = board.fen()
+            board_fens.append(fen_string)
 
-                while True:
-                    fen_string = board.fen()
+            # Perform mcts simulations
+            for _ in range(self.mcts_simulations):
+                self.mcts.search(board, nnet)
 
-                    # Perform mcts simulations
-                    for _ in range(self.mcts_simulations):
-                        self.mcts.search(board, nnet)
+            # Gets the moves and policy from the mcts, as well as the individual move to take
+            # NOTE: moves[i] corresponds to search_probs[i]
+            moves, search_probs, move = self.mcts.find_search_probs(fen_string, temperature=5)
 
-                    # Gets the moves and policy from the mcts, as well as the individual move to take
-                    # NOTE: moves[i] corresponds to search_probs[i]
-                    moves, search_probs, move = self.mcts.find_search_probs(fen_string, temperature=5)
-            
-                    # Store board state (used in training loop)
-                    self.board_fens.append(fen_string)
+            # Converts mcts search probabilites to (8,8,73) vector
+            full_search_probs = self.policy_converter.compute_full_search_probs(moves, search_probs, board)
+            mcts_probs.append(full_search_probs)
 
-                    # Converts mcts search probabilites to (8,8,73) vector
-                    full_search_probs = self.policy_converter.compute_full_search_probs(moves, search_probs, board)
-                    
-                    # Adds entry to the training examples
-                    self.mcts_probs.append(full_search_probs)
+            # Makes the random action on the board, and gets fen string
+            move = chess.Move.from_uci(move)
+            board.push(move)
 
-                    # Makes the random action on the board, and gets fen string
-                    move = chess.Move.from_uci(move)
-                    board.push(move)
-                    # print(board)
+            # If the game is over, end the episode
+            # TODO: Consider removing `board.can_claim_draw()` as it may be slow to check.
+            # See https://python-chess.readthedocs.io/en/latest/core.html#chess.Board.can_claim_draw
+            if board.is_game_over() or board.can_claim_draw():
+                break
 
-                    # If the game is over, end the episode
-                    # TODO: Consider removing `board.can_claim_draw()` as it may be slow to check.
-                    # See https://python-chess.readthedocs.io/en/latest/core.html#chess.Board.can_claim_draw
-                    if board.is_game_over() or board.can_claim_draw():
-                        self.mcts_evals = torch.zeros(len(self.mcts_probs))
-                        self.assign_rewards(board)
-                        break
+        state_values = self.assign_rewards(board, len(mcts_probs))
+        return board_fens, state_values, mcts_probs
 
-                self.mcts_probs = torch.tensor(np.array(self.mcts_probs)).float()
-                self.mcts_evals = self.mcts_evals.float()
-
-                # Create iterable dataset with mcts labels
-                dataset = TensorDataset(self.mcts_probs, self.mcts_evals)
-                train_dl = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False)
-
-                # Define loss functions
-                ce_loss_fn = torch.nn.CrossEntropyLoss()
-                mse_loss_fn = torch.nn.MSELoss()
-
-                # Create optimizer for updating parameters during training.
-                opt = torch.optim.SGD(nnet.parameters(), lr=self.lr, weight_decay=0.001, momentum=0.9)
-
-                # Training Loop
-                for _ in range(epochs):
-                    losses = []
-                    for batch_index, (mcts_probs, mcts_evals) in enumerate(train_dl):
-                        with torch.enable_grad():
-                            # Get the batch size based on the batch size of the training dataloader
-                            batch_size = mcts_evals.size(dim=0)
-
-                            policy_batch = []
-                            value_batch = []
-                            
-                            # Store policies and values for entire batch
-                            for i in range(batch_size):
-                                policy, value = nnet(get_cnn_input(chess.Board(self.board_fens[i + batch_index * 8])))
-                                policy_batch.append(policy)
-                                value_batch.append(value)
-
-                            # Convert the list of tensors to a single tensor for policy and value.
-                            policy_batch = torch.stack(policy_batch).float()
-                            value_batch = torch.stack(value_batch).flatten().float()
-
-                            # Find the loss and store it
-                            loss = ce_loss_fn(policy_batch, mcts_probs) + mse_loss_fn(value_batch, mcts_evals)
-                            losses.append(loss.item())
-
-                            # Calculate Gradients
-                            loss.backward()
-
-                            # Update parameters
-                            opt.step()
-
-                            # Reset gradients
-                            opt.zero_grad()
-
-                    print(losses)
-            
-    def assign_rewards(self, board):
+    def assign_rewards(self, board, length):
         """Iterates through training examples and assigns rewards based on result of the game.
         """
         reward = 0
+        values = [0 for _ in range(length)]
+
         if (board.outcome() is not None) and (board.outcome().winner is not None):
             reward = -1
-        for move_num in range(len(self.mcts_evals) - 1, -1, -1):
+        for move_num in range(length - 1, -1, -1):
             reward *= -1
-            self.mcts_evals[move_num] = reward
+            values[move_num] = reward
 
         # For demonstration print the board and outcome
         # print(board)
-        print(self.mcts_evals)
+        print(values)
         print(board.outcome())
+        return values
+
+    def training_episode(self, nnet, games, epochs, batch_size):
+        """Builds dataset from given number of training games and current network,
+        then trains the network on the MCTS output and game outcomes.
+        """
+
+        with torch.no_grad():
+            # Obtain data from games and separate into appropriate lists
+            game_data = [self.training_game(nnet) for _ in range(games)]
+            # board_fens, state_values, mcts_probs = reduce(lambda g1, g2: (torch.cat(x,y) for x, y in zip(g1, g2)), game_data)
+            board_fens, state_values, mcts_probs = (x+y for x, y in zip(game_data[0], game_data[1]))
+
+            inputs = torch.stack([get_cnn_input(chess.Board(fen)) for fen in board_fens])
+            mcts_probs = torch.tensor(np.array(mcts_probs)).float()
+            state_values = torch.tensor(np.array(state_values)).float()
+
+            # Create iterable dataset with mcts labels
+            dataset = TensorDataset(inputs, state_values, mcts_probs)
+            train_dl = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False)
+
+            # Define loss functions
+            ce_loss_fn = torch.nn.CrossEntropyLoss()
+            mse_loss_fn = torch.nn.MSELoss()
+
+            # Create optimizer for updating parameters during training.
+            opt = torch.optim.SGD(nnet.parameters(), lr=self.lr, weight_decay=0.001, momentum=0.9)
+
+            # Training Loop
+            for _ in range(epochs):
+                losses = []
+                for (inputs, state_values, mcts_probs) in train_dl:
+                    with torch.enable_grad():
+                        policy_batch = []
+                        value_batch = []
+
+                        # Store policies and values for entire batch
+                        for state in inputs:
+                            policy, value = nnet(state)
+                            policy_batch.append(policy)
+                            value_batch.append(value)
+
+                        # Convert the list of tensors to a single tensor for policy and value.
+                        policy_batch = torch.stack(policy_batch).float()
+                        value_batch = torch.stack(value_batch).flatten().float()
+
+                        # Find the loss and store it
+                        loss = ce_loss_fn(policy_batch, mcts_probs) + mse_loss_fn(value_batch, state_values)
+                        losses.append(loss.item())
+
+                        # Calculate Gradients
+                        loss.backward()
+
+                        # Update parameters
+                        opt.step()
+
+                        # Reset gradients
+                        opt.zero_grad()
+
+                print(losses)
 
 
 def main():
-    # Gets the neural network, and performs and episode
+    # Gets the neural network, and performs episodes
     nnet = PlayNetwork()
-    train = MctsTrain(mcts_simulations=5, exploration=5, lr=0.1)
+    train = MctsTrain(mcts_simulations=3, exploration=5, lr=0.1)
 
-    # TODO: Make training support epochs
     for _ in range(10):
-        train.training_episode(nnet, 1)
+        train.training_episode(nnet, 2, 2, 8)
 
 
 if __name__ == "__main__":
