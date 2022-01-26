bc1a108a24030de3612ef48bd03fb72cd89b57d7
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2022-01-26 11:33:01 -0500

    Added missing comma

--- firmware/raspi/status.py
+++ firmware/raspi/status.py
@@ -35,8 +35,8 @@ class ArduinoStatus:
     def is_valid_code(self, code):
         return code in (ArduinoStatus.IDLE,
                     ArduinoStatus.EXECUTING_MOVE,
-                    ArduinoStatus.END_TURN_BUTTON_PRESSED
-                    ArduinoStatus.ERROR)   
+                    ArduinoStatus.END_TURN_BUTTON_PRESSED,
+                    ArduinoStatus.ERROR) 
 
 class Status:
     '''Helper class that stores current status of game, along with related metadata.
