186213b20248dd59567ec634dd45294bde3ff3fd
JuddB <jbrockedgar@gmail.com>
2022-01-31 14:56:42 -0500

    Update checkpointing.py

--- chess-AI/checkpointing.py
+++ chess-AI/checkpointing.py
@@ -1,9 +1,13 @@
 import torch
 
 def save_model(nnet):
+    '''Save given model parameters to external file
+    '''
     torch.save(nnet.state_dict(), 'chess-AI/models.pt')
 
 def load_model(nnet):
+    '''Load model parameters into given network from external file
+    '''
     nnet.load_state_dict(torch.load('chess-AI/models.pt'))
     return nnet
 
@@ -12,3 +16,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
