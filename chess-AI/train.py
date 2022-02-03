55a800a4e46b3449b5b236284691ca4a24d77717
fshcat <jhoyos314@gmail.com>
2022-02-02 19:59:35 -0500

    Added TrainOptions class and fixed some comments

--- chess-AI/train.py
+++ chess-AI/train.py
@@ -13,20 +13,26 @@ from output_representation import PlayNetworkPolicyConverter
 from state_representation import get_cnn_input
 
 
+class TrainOptions:
+    """This class stores settings for the training episodes"""
+
+    def __init__(self, games, lr, epochs, batch_size):
+        self.lr = lr
+        self.games = games
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+
 class Train:
-    """This class is used to run the monte carlo simulations and drive the model training.
+    """This class is used to run the model training.
 
     Attributes:
-        mcts_simulations: Number of simulations to use for MCTS
-        training_examples: List of all the training examples to be used
-        mcts: References the MCTS class
+        move_approximator: Gets approximate best moves from a position
+        value_approximator: Gets approximate value of a position
         policy_converter: References the PlayNetworkPolicyConverter class
     """
-    def __init__(self, lr, move_approximator, save_path, val_approximator=None):
+    def __init__(self, move_approximator, save_path, val_approximator=None):
         self.policy_converter = PlayNetworkPolicyConverter()
-
-        # Learning rate
-        self.lr = lr
 
         # board -> move_list, move_probabilities, move_to_take
         self.move_approximator = move_approximator
@@ -63,7 +69,7 @@ class Train:
             # NOTE: moves[i] corresponds to search_probs[i]
             moves, move_probs, move = self.move_approximator(board)
 
-            # Converts mcts search probabilites to (8,8,73) vector
+            # Converts move probabilites to (8,8,73) vector
             move_probs_vector = self.policy_converter.compute_full_search_probs(moves, move_probs, board)
             all_move_probs.append(move_probs_vector)
 
@@ -103,14 +109,14 @@ class Train:
         print(board.outcome())
         return values
 
-    def training_episode(self, nnet, games, epochs, batch_size, num_saved_models, overwrite_save):
+    def training_episode(self, nnet, options, num_saved_models, overwrite_save):
         """Builds dataset from given number of training games and current network,
-        then trains the network on the MCTS output and game outcomes.
+        then trains the network on the output and game outcomes.
         """
 
         with torch.no_grad():
             # Obtain data from games and separate into appropriate lists
-            game_data = [self.training_game() for _ in range(games)]
+            game_data = [self.training_game() for _ in range(options.games)]
             board_fens, state_values, move_probs = reduce(lambda g1, g2: (x+y for x, y in zip(g1, g2)), game_data)
 
             inputs = torch.stack([get_cnn_input(chess.Board(fen)) for fen in board_fens])
@@ -119,17 +125,17 @@ class Train:
 
             # Create iterable dataset from game data
             dataset = TensorDataset(inputs, state_values, move_probs)
-            train_dl = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False)
+            train_dl = DataLoader(dataset=dataset, batch_size=options.batch_size, shuffle=False)
 
             # Define loss functions
             ce_loss_fn = torch.nn.CrossEntropyLoss()
             mse_loss_fn = torch.nn.MSELoss()
 
             # Create optimizer for updating parameters during training.
-            opt = torch.optim.SGD(nnet.parameters(), lr=self.lr, weight_decay=0.001, momentum=0.9)
+            opt = torch.optim.SGD(nnet.parameters(), lr=options.lr, weight_decay=0.001, momentum=0.9)
 
             # Training Loop
-            for _ in range(epochs):
+            for _ in range(options.epochs):
                 losses = []
                 for (inputs, state_values, move_probs) in train_dl:
                     with torch.enable_grad():
@@ -190,14 +196,14 @@ def main():
                 if i != 0:
                     nnet = load_model(nnet, f'chess-AI/models-{i+1}.pt')
                 break
-        
 
     # Partially applies parameters to mcts function
     mcts_moves = lambda board: mcts.get_tree_results(mcts_simulations, nnet, board, temperature=5)
 
-    train = Train(lr=0.2, move_approximator=mcts_moves, save_path=None)
+    options = TrainOptions(games=3, lr=0.2, epochs=3, batch_size=10)
+    train = Train(move_approximator=mcts_moves, save_path=None)
 
-    train.training_episode(nnet, games=3, epochs=3, batch_size=10, num_saved_models=num_saved_models, overwrite_save=overwrite_save)
+    train.training_episode(nnet, options, num_saved_models=num_saved_models, overwrite_save=overwrite_save)
 
 
 if __name__ == "__main__":
