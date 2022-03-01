4a422c3d9a0afe9ee803a01a4722295d8bd34235
Nashir Janmohamed <nashirbass@gmail.com>
2022-02-28 20:57:14 -0500

    Move main loop logic to a process function

--- firmware/raspi/game.py
+++ firmware/raspi/game.py
@@ -29,15 +29,11 @@ def is_human_turn_at_start():
             return True
         print("Please choose one of [w], [b], or [r].")
 
-def handle_human_move(mode_of_interaction, board):
+def get_human_move(mode_of_interaction, board):
     '''Handle human move based on specified mode of interaction.
     '''
     if mode_of_interaction == 'cli':
-        uci_move = CLHumanPlayer.select_move(board)
-        try:
-            board.send_move_to_board(uci_move)
-        except NotImplementedError as nie:
-            print(nie.__str__())
+        return CLHumanPlayer.select_move(board)
     elif mode_of_interaction == 'over_the_board':
         # TODO: think about handling backfill of promotion area if person made a promotion move.
         # If needed, backfill the promotion area (if possible).
@@ -46,13 +42,14 @@ def handle_human_move(mode_of_interaction, board):
     else:
         raise ValueError("Other modes of interaction are unimplemented")
 
-def handle_ai_move(ai_player, board):
+def get_ai_move(ai_player, board):
     '''Handle AI move.
     '''
-    uci_move = ai_player.select_move(board.engine.fen())
+    return ai_player.select_move(board.engine.fen())
+
+def send_move_to_board(uci_move, board):
     try:
         board.send_move_to_board(uci_move)
-        print(f"AI made move: {uci_move}")
     except NotImplementedError as nie:
         print(nie.__str__())
 
@@ -68,9 +65,67 @@ def player_wants_rematch():
     # TODO: implement
     return False
 
-# TODO: abstract main program logic in here
-def run_user_interaction_loop():
-    pass
+# TODO: convert to class based and store all passed parameters as class members
+def process(board, is_human_turn, mode_of_interaction, ai_player):
+    '''One iteration of main game loop.
+
+    Returns:
+        is_human_turn: boolean that is True if it is human turn. Return None if game is over.
+    '''
+    # TODO: Handle game end condition here, rematch, termination, etc.
+    if board.engine.is_game_over():
+        # If game is over, return None for is_human_turn
+        return None
+
+    board_status = board.get_status_from_arduino()
+    print(f"Board Status: {board_status}")
+
+    if board_status.status == ArduinoStatus.EXECUTING_MOVE:
+        # Wait for move in progress to finish executing
+        time.sleep(1) # reduce the amount of polling while waiting for move to finish
+
+        # TODO: This is just so we have game loop working, remove once we read from arduino
+        board.set_status_from_arduino(ArduinoStatus.IDLE,
+                                      board.msg_queue[0].move_count % 10,
+                                      None)
+        # Turn doesn't change, since we don't get next move if Arduino is still executing
+        return is_human_turn
+
+    if board_status.status == ArduinoStatus.ERROR:
+        # TODO: figure out edge/error cases and handle them here
+        raise ValueError("Unimplemented, need to handle errors")
+
+    if board_status.status == ArduinoStatus.IDLE:
+        if board.msg_queue:
+            # Arduino sends and receives move_count % 10, since it can only transmit one char for
+            # move count
+            if all([board_status.move_count == board.msg_queue[0].move_count % 10,
+                    board_status.status == ArduinoStatus.IDLE]):
+                board.msg_queue.popleft()
+
+        if board.msg_queue:
+            board.dispatch_msg_from_queue()
+            # If moves still in queue, we just try to empty queue, don't get any new move
+            return is_human_turn
+
+        board.show_on_cli()
+
+        if is_human_turn:
+            uci_move = get_human_move(mode_of_interaction, board)
+        else:
+            uci_move = get_ai_move(ai_player, board)
+
+        send_move_to_board(uci_move, board)
+
+        player = "Player" if is_human_turn else "AI"
+        print(f"{player} made move: {uci_move}")
+        # TODO: After every move, center piece that was just moved on its new square. Need to
+        # account for castles as well.
+
+        # Status.write_game_status_to_disk(board)
+
+        # If we got here, we added a new move to the move queue, so we flip turn
+        return not is_human_turn
 
 # TODO: abstract test file logic in here
 def run_test_file():
@@ -117,9 +172,10 @@ def main():
     mode_of_interaction, is_human_turn, board, ai_player = init_parameters()
     
     # Main game loop
-    while True:
-        # TODO: Handle game end condition here, rematch, termination, etc.
-        if board.engine.is_game_over():
+    while is_human_turn is not None:
+        is_human_turn = process(board, is_human_turn, mode_of_interaction, ai_player)
+
+        if is_human_turn is None:
             if not player_wants_rematch():
                 print("Thanks for playing")
                 reset_board()
@@ -127,47 +183,6 @@ def main():
 
             print("Ok, resetting board")
             reset_board()
-
-        board_status = board.get_status_from_arduino()
-        print(f"Board Status: {board_status}")
-
-        if board_status.status == ArduinoStatus.EXECUTING_MOVE:
-            # Wait for move in progress to finish executing
-            time.sleep(1) # reduce the amount of polling while waiting for move to finish
-
-            # TODO: This is just so we have game loop working, remove once we read from arduino
-            board.set_status_from_arduino(ArduinoStatus.IDLE,
-                                          board.msg_queue[0].move_count % 10,
-                                          None)
-            continue
-
-        if board_status.status == ArduinoStatus.ERROR:
-            # TODO: figure out edge/error cases and handle them here
-            raise ValueError("Unimplemented, need to handle errors")
-
-        if board_status.status == ArduinoStatus.IDLE:
-            if board.msg_queue:
-                # Arduino sends and receives move_count % 10, since it can only transmit one char for
-                # move count
-                if all([board_status.move_count == board.msg_queue[0].move_count % 10,
-                        board_status.status == ArduinoStatus.IDLE]):
-                    board.msg_queue.popleft()
-
-            if board.msg_queue:
-                board.dispatch_msg_from_queue()
-                continue
-
-            board.show_on_cli()
-
-            if is_human_turn:
-                handle_human_move(mode_of_interaction, board)
-            else:
-                handle_ai_move(ai_player, board)
-            # TODO: After every move, center piece that was just moved on its new square. Need to
-            # account for castles as well.
-
-            is_human_turn = not is_human_turn
-            # Status.write_game_status_to_disk(board)
 
 if __name__ == '__main__':
     main()
