10c5c55874de804414f7d391b7c07a1319f2039f
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-04-07 19:08:26 -0400

    Merging

--- chess-AI/experimental-work/figshare_upload.py
+++ chess-AI/experimental-work/figshare_upload.py
@@ -9,142 +9,120 @@ from requests.exceptions import HTTPError
 
 BASE_URL = 'https://api.figshare.com/v2/{endpoint}'
 
+# 10MB chunks of data
 CHUNK_SIZE = 1048576
+
+# get the token that is an environment variable
 TOKEN = os.getenv('FIGSHARE_KEY')
-print(TOKEN)
-# FILE_PATH = '/path/to/work/directory/cat.obj'
-# TITLE = 'A 3D cat object model'
+
+# specify file that should be uploaded
+FILE_PATH = '../models.pt'
+TITLE = 'Model data'
 
 
-# def raw_issue_request(method, url, data=None, binary=False):
-#     headers = {'Authorization': 'token ' + TOKEN}
-#     if data is not None and not binary:
-#         data = json.dumps(data)
-#     response = requests.request(method, url, headers=headers, data=data)
-#     try:
-#         response.raise_for_status()
-#         try:
-#             data = json.loads(response.content)
-#         except ValueError:
-#             data = response.content
-#     except HTTPError as error:
-#         print ("Caught an HTTPError: {}".format(error.message))
-#         print ('Body:\n', response.content)
-#         raise
+def raw_issue_request(method, url, data=None, binary=False):
+    headers = {'Authorization': 'token ' + TOKEN}
+    if data is not None and not binary:
+        data = json.dumps(data)
+    response = requests.request(method, url, headers=headers, data=data)
+    try:
+        response.raise_for_status()
+        try:
+            data = json.loads(response.content)
+        except ValueError:
+            data = response.content
+    except HTTPError as error:
+        print ("Caught an HTTPError: {}".format(error.message))
+        print ('Body:\n', response.content)
+        raise
 
-#     return data
+    return data
 
-
-# def issue_request(method, endpoint, *args, **kwargs):
-#     return raw_issue_request(method, BASE_URL.format(endpoint=endpoint), *args, **kwargs)
-
-
-# def list_articles():
-#     result = issue_request('GET', 'account/articles')
-#     print ('Listing current articles:')
-#     if result:
-#         for item in result:
-#             print (u'  {url} - {title}'.format(**item))
-#     else:
-#         print ('  No articles.')
-#     print
+def issue_request(method, endpoint, *args, **kwargs):
+    return raw_issue_request(method, BASE_URL.format(endpoint=endpoint), *args, **kwargs)
 
 
-# def create_article(title):
-#     data = {
-#         'title': title  # You may add any other information about the article here as you wish.
-#     }
-#     result = issue_request('POST', 'account/articles', data=data)
-#     print ('Created article:', result['location'], '\n')
+# create the article within figshare
+def create_article(title):
+    data = {
+        'title': title 
+    }
+    result = issue_request('POST', 'account/articles', data=data)
+    print ('Created article:', result['location'], '\n')
 
-#     result = raw_issue_request('GET', result['location'])
-
-#     return result['id']
-
-
-# def list_files_of_article(article_id):
-#     result = issue_request('GET', 'account/articles/{}/files'.format(article_id))
-#     print ('Listing files for article {}:'.format(article_id))
-#     if result:
-#         for item in result:
-#             print ('  {id} - {name}'.format(**item))
-#     else:
-#         print ('  No files.')
-
-#     print
+    result = raw_issue_request('GET', result['location'])
+    return result['id']
 
 
-# def get_file_check_data(file_name):
-#     with open(file_name, 'rb') as fin:
-#         md5 = hashlib.md5()
-#         size = 0
-#         data = fin.read(CHUNK_SIZE)
-#         while data:
-#             size += len(data)
-#             md5.update(data)
-#             data = fin.read(CHUNK_SIZE)
-#         return md5.hexdigest(), size
+# ensure file is good to be uploaded
+def get_file_check_data(file_name):
+    with open(file_name, 'rb') as fin:
+        md5 = hashlib.md5()
+        size = 0
+        data = fin.read(CHUNK_SIZE)
+        while data:
+            size += len(data)
+            md5.update(data)
+            data = fin.read(CHUNK_SIZE)
+        return md5.hexdigest(), size
 
 
-# def initiate_new_upload(article_id, file_name):
-#     endpoint = 'account/articles/{}/files'
-#     endpoint = endpoint.format(article_id)
+# upload the file to figshare
+def initiate_new_upload(article_id, file_name):
+    endpoint = 'account/articles/{}/files'
+    endpoint = endpoint.format(article_id)
 
-#     md5, size = get_file_check_data(file_name)
-#     data = {'name': os.path.basename(file_name),
-#             'md5': md5,
-#             'size': size}
+    md5, size = get_file_check_data(file_name)
+    data = {'name': os.path.basename(file_name),
+            'md5': md5,
+            'size': size}
 
-#     result = issue_request('POST', endpoint, data=data)
-#     print ('Initiated file upload:', result['location'], '\n')
+    result = issue_request('POST', endpoint, data=data)
+    print ('Initiated file upload:', result['location'], '\n')
 
-#     result = raw_issue_request('GET', result['location'])
+    result = raw_issue_request('GET', result['location'])
 
-#     return result
-
-
-# def complete_upload(article_id, file_id):
-#     issue_request('POST', 'account/articles/{}/files/{}'.format(article_id, file_id))
+    return result
 
 
-# def upload_parts(file_info):
-#     url = '{upload_url}'.format(**file_info)
-#     result = raw_issue_request('GET', url)
+def complete_upload(article_id, file_id):
+    issue_request('POST', 'account/articles/{}/files/{}'.format(article_id, file_id))
 
-#     print('Uploading parts:')
-#     with open(FILE_PATH, 'rb') as fin:
-#         for part in result['parts']:
-#             upload_part(file_info, fin, part)
-#     print
+def upload_parts(file_info):
+    url = '{upload_url}'.format(**file_info)
+    result = raw_issue_request('GET', url)
 
-
-# def upload_part(file_info, stream, part):
-#     udata = file_info.copy()
-#     udata.update(part)
-#     url = '{upload_url}/{partNo}'.format(**udata)
-
-#     stream.seek(part['startOffset'])
-#     data = stream.read(part['endOffset'] - part['startOffset'] + 1)
-
-#     raw_issue_request('PUT', url, data=data, binary=True)
-#     print ('  Uploaded part {partNo} from {startOffset} to {endOffset}'.format(**part))
+    print('Uploading parts:')
+    with open(FILE_PATH, 'rb') as fin:
+        for part in result['parts']:
+            upload_part(file_info, fin, part)
+    print
 
 
-# def main():
-#     # We first create the article
-#     list_articles()
-#     article_id = create_article(TITLE)
-#     list_articles()
-#     list_files_of_article(article_id)
+def upload_part(file_info, stream, part):
+    udata = file_info.copy()
+    udata.update(part)
+    url = '{upload_url}/{partNo}'.format(**udata)
 
-#     # Then we upload the file.
-#     file_info = initiate_new_upload(article_id, FILE_PATH)
-#     # Until here we used the figshare API; following lines use the figshare upload service API.
-#     upload_parts(file_info)
-#     # We return to the figshare API to complete the file upload process.
-#     complete_upload(article_id, file_info['id'])
-#     list_files_of_article(article_id)
+    stream.seek(part['startOffset'])
+    data = stream.read(part['endOffset'] - part['startOffset'] + 1)
+
+    raw_issue_request('PUT', url, data=data, binary=True)
+    print ('  Uploaded part {partNo} from {startOffset} to {endOffset}'.format(**part))
 
 
-# if __name__ == '__main__':
-#     main()
+def main():
+    
+    # create the article
+    article_id = create_article(TITLE)
+
+    # Then we upload the file.
+    file_info = initiate_new_upload(article_id, FILE_PATH)
+
+    upload_parts(file_info)
+
+    # We return to the figshare API to complete the file upload process.
+    complete_upload(article_id, file_info['id'])
+
+if __name__ == '__main__':
+    main()
