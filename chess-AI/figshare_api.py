c71b5364684acb766b0eb08a593f1fc648546e0e
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-04-10 18:09:12 -0400

    Cleaning up and adding comments

--- chess-AI/figshare_api.py
+++ chess-AI/figshare_api.py
@@ -1,55 +1,65 @@
-#!/usr/bin/env python
-import os
+"""Module for retrieving and uploading articles through the
+Figshare API
+"""
+
 import hashlib
 import json
 import os
 
 from urllib.request import urlretrieve
-import torch
 import requests
 from requests.exceptions import HTTPError
 
 class FigshareApi:
-
+    """ Class with al the functions for interacting with Figshare
+    """
+    # Size of upload chunks
     CHUNK_SIZE = 1048576
+    #  Figshare base UR
     BASE_URL = 'https://api.figshare.com/v2/{endpoint}'
 
     def __init__(self):
-        # get the api key which is stored as an environment variable
+        # API key needs to be stored as an environment variable
         self.api_key = os.getenv('FIGSHARE_KEY')
-            
-    def get_figshare_article(self,store_path,base_path,file_name):
-        """Get a dataset from figshare using the path provided"""
-        # api endpoint
+
+    def get_figshare_article(self,store_path,file_name):
+        """Get an article from figshare and store it in
+        store_path.
+
+        file_name refers to the name of the file that is in
+        figshare, not the title
+        """
+
         endpoint = "account/articles/{article_id}/files"
         download_url = None
-        # get all the articles within figshare
-        results = self.get_articles()
-        # find the file the we are are looking for
-        for item in results:            
-            # find the figshare item with matching title
-            article_id = item['id']
-            # get the data from the file
+        # Get all the articles within figshare
+        articles = self.get_articles()
+        # Iterate through all the returned articles
+        for article in articles:
+            article_id = article['id']
             file_data = self.issue_request('GET', endpoint.format(article_id=article_id))
-            # ensure file data is not empty
+            # Ensure articles has associated file data
             if len(file_data) > 0:
+                # Get the file name of the article
                 figshare_file = file_data[0]['name']
-                # check to see if file has the same name
+                # See if this the file being searched for
                 if figshare_file == file_name:
+                    # Get the download URL
                     download_url = file_data[0]["download_url"]
                     break
-        # means no file was found        
         if not download_url:
             print("File %s not found in Figshare" % (file_name))
             return False
-        # found file in figshare    
-        else:        
-            urlretrieve(download_url, store_path)
-            print("File %s downloaded to %s" % (file_name,store_path))
-        # return true to signify file was found    
+           
+        urlretrieve(download_url, store_path)
+        print("File %s downloaded to %s" % (file_name,store_path))
+        # Return true to signify file was found 
         return True
 
     def raw_issue_request(self,method, url, data=None, binary=False):
+        """ Utility function taken from Figshare documentation
+        """
+
         headers = {'Authorization': 'token ' + self.api_key}
         if data is not None and not binary:
             data = json.dumps(data)
@@ -68,11 +78,20 @@ class FigshareApi:
         return data
 
     def issue_request(self,method, endpoint, *args, **kwargs):
-        return self.raw_issue_request(method, self.BASE_URL.format(endpoint=endpoint), *args, **kwargs)
+        """ Utility function taken from Figshare documentation
+        """
+        return self.raw_issue_request(method, self.BASE_URL.format(endpoint=endpoint), \
+        *args, **kwargs)
 
     # create the article within figshare
     def create_article(self,title,desc,keywords,categories):
-        # NOTE: this data is just place holder , however required for publishing
+        """ Create an article given the title, description, keywords
+        and categories
+
+        These are all fields that are required for an article to be published
+        in Figshare
+        """
+
         data = {
             'title': title,
             "description": desc,
@@ -86,6 +105,9 @@ class FigshareApi:
         return result['id']
 
     def get_file_check_data(self,file_name):
+        """ Utility function taken from Figshare documentation
+        """
+
         with open(file_name, 'rb') as fin:
             md5 = hashlib.md5()
             size = 0
@@ -98,6 +120,9 @@ class FigshareApi:
             return md5.hexdigest(), size
 
     def initiate_new_upload(self,article_id, file_name):
+        """ Utility function taken from Figshare documentation
+        """
+
         endpoint = 'account/articles/{}/files'
         endpoint = endpoint.format(article_id)
 
@@ -115,13 +140,21 @@ class FigshareApi:
         return result
 
     def get_articles(self):
+        """ Get all the articles in Figshare
+        """
+
         result = self.issue_request('GET', 'account/articles')
         return result
 
     def complete_upload(self,article_id, file_id):
+        """ Utility function taken from Figshare documentation
+        """
         self.issue_request('POST', 'account/articles/{}/files/{}'.format(article_id, file_id))
 
     def upload_parts(self,file_info,path):
+        """ Utility function taken from Figshare documentation
+        """
+
         url = '{upload_url}'.format(**file_info)
         result = self.raw_issue_request('GET', url)
 
@@ -130,9 +163,11 @@ class FigshareApi:
         with open(path, 'rb') as fin:
             for part in result['parts']:
                 self.upload_part(file_info, fin, part)
-        print
 
     def upload_part(self,file_info, stream, part):
+        """ Utility function taken from Figshare documentation
+        """
+
         udata = file_info.copy()
         udata.update(part)
         url = '{upload_url}/{partNo}'.format(**udata)
@@ -144,15 +179,17 @@ class FigshareApi:
         print ('  Uploaded part {partNo} from {startOffset} to {endOffset}'.format(**part))
 
     def upload(self,title,desc,keywords,categories,path):
-        # create and upload the article
+        """ Upload and publish an article to figshare
+
+        Articles do not become accessible through the API until they
+        are published
+        """
+
+        # Create the article and get associated ID
         article_id = self.create_article(title,desc,keywords,categories)
+        # Upload the file to Figshare
         file_info = self.initiate_new_upload(article_id, path)
         self.upload_parts(file_info,path)
-        # We return to the figshare API to complete the file upload process.
         self.complete_upload(article_id, file_info['id'])
-        # article must be published to be downloaded
-        self.issue_request('POST', 'account/articles/{}/publish'.format(article_id))
-
-
-       
-
+        # The article must be published
+        self.issue_request('POST', f"account/articles/{article_id}/publish".format(article_id))
