5ec03c5db5aa0594f916aa124fa829b6ddb79b28
RuthShryock <ruthieshryock@gmail.com>
2022-04-19 12:39:45 -0400

    adding GUIPlayer methods and styling code

--- firmware/raspi/game.py
+++ firmware/raspi/game.py
@@ -50,8 +50,9 @@ class Game:
     def last_made_move(self):
         """Returns the last made move, if applicable. If no moves have been made, returns None.
         """
-        '''if self.board.engine.chess_board.move_stack:
-            return None'''#this was always defaulting to true and not allowing access to move
+        """if self.board.engine.chess_board.move_stack:
+            return None"""
+        #this was always defaulting to true and not allowing access to move
         return self.board.engine.chess_board.peek().uci()
 
     def is_game_over(self):
@@ -78,7 +79,7 @@ class Game:
             raise NotImplementedError("Need to handle case of invalid move input. "
                                       "Should we loop until move is valid? What if "
                                       "the board is messed up? Need to revisit.")
-    
+
     def process(self, player):
         """One iteration of main game loop.
 
@@ -89,9 +90,6 @@ class Game:
         Returns:
             made_move: boolean that is True if turn changes, otherwise False.
         """
-        print("this is our player:", player)
-        print("the turn is white: ", self.board.engine.is_white_turn())
-        print("this is the message queue: ", self.board.msg_queue)
         arduino_status = self.board.get_status_from_arduino()
         print(f"\nBoard Status: {arduino_status}")
 
@@ -124,7 +122,6 @@ class Game:
                     return False
 
             if self.board.msg_queue:
-                print("we are using a move from the queue")
                 self.board.dispatch_msg_from_queue()
                 # If moves still in queue, we just try to empty queue, don't get any new move
                 return False
@@ -175,100 +172,3 @@ class Game:
             return True
 
         raise ValueError("We shouldn't reach this point in the function.")
-
-    def processM(self, move):
-            """One iteration of main game loop.
-
-            Note: expects caller to check game.is_game_over before calling.
-            There may be moves remaining on self.board.msg_queue after game is over, so it is legal to
-                call process after is_game_over returns True as long as board.msg_queue is nonempty.
-
-            Returns:
-                made_move: boolean that is True if turn changes, otherwise False.
-            """
-            print("the turn is white: ", self.board.engine.is_white_turn())
-            arduino_status = self.board.get_status_from_arduino()
-            print(f"\nBoard Status: {arduino_status}")
-
-            if arduino_status.status == status.ArduinoStatus.EXECUTING_MOVE:
-                # Wait for move in progress to finish executing
-                time.sleep(1) # reduce the amount of polling while waiting for move to finish
-
-                if self.board.ser is None:
-                    # Allows testing other game loop functionality with simulated connection to Arduino
-                    self.board.set_status_from_arduino(status.ArduinoStatus.IDLE,
-                                                    self.board.msg_queue[0].move_count,
-                                                    self.board.msg_queue[0].op_code)
-                # Turn doesn't change, since we don't get next move if Arduino is still executing
-                return False
-
-            if arduino_status.status == status.ArduinoStatus.ERROR:
-                # TODO: figure out edge/error cases and handle them here
-                raise ValueError("Unimplemented, need to handle errors")
-
-            if arduino_status.status == status.ArduinoStatus.IDLE:
-                if self.board.msg_queue:
-                    # We have a separate move counter for moves and instructions; to resolve conflicts
-                    # we check both OpType (to identify whether we are checking for an instruction or
-                    # move, and then the move number. If both match, we remove the message from queue.
-                    if all([arduino_status.extra == self.board.msg_queue[0].op_code,
-                            arduino_status.move_count == self.board.msg_queue[0].move_count]):
-                        self.board.msg_queue.popleft()
-                        print("Removed message from queue")
-                        # After removing move from queue, return, allows rechecking if msg_queue empty
-                        return False
-
-                if self.board.msg_queue:
-                    self.board.dispatch_msg_from_queue()
-                    # If moves still in queue, we just try to empty queue, don't get any new move
-                    return False
-
-                # Takes the move as input for function
-                msg = move
-
-                if msg is None:
-                    # If msg is None, we have run out of moves in our test file, returning None
-                    # indicates this is the case.
-                    return None
-                # msg can be a UCI move, or it can be a fully formatted `Message` string
-                if len(msg) in (4, 5):
-                    self.send_uci_move_to_board(msg)
-                elif len(msg) == status.OpCode.MESSAGE_LENGTH:
-                    # Decompose each move into a `Message` type and add to board's msg queue
-                    op_code = msg[1]
-                    if op_code in status.OpCode.UCI_MOVE_OPCODES:
-                        # TODO: need to handle promotions here; maybe also needs to be updated
-                        # in general for the opcodes. This section has to be more fleshed out.
-                        start_bc = util.BoardCell(ord(msg[2]) - ord('A'), ord(msg[3]) - ord('A'))
-                        end_bc = util.BoardCell(ord(msg[4]) - ord('A'), ord(msg[5]) - ord('A'))
-                        # If black pieces are facing player, need to transpose board cells.
-                        if not self.board.human_plays_white_pieces:
-                            start_bc = util.transpose_boardcell(start_bc)
-                            end_bc = util.transpose_boardcell(end_bc)
-                        # TODO: if human_plays_white_pieces is false, need to flip boardcell diagonally
-                        self.board.add_move_to_queue(start_bc, end_bc, op_code)
-                        return False
-                    if op_code == status.OpCode.INSTRUCTION:
-                        # Note: Instruction type opcodes are added to front of queue as they take
-                        # priority. Ex: want to request home axis before sending any other msgs.
-                        self.board.add_instruction_to_queue(op_type=msg[2], extra=msg[3])
-                        return False
-                    raise ValueError(f"Couldn't parse message {msg}")
-
-                else:
-                    raise ValueError(f"Received invalid message {msg}")
-
-                # TODO: After every move, center piece that was just moved on its new square. Need to
-                # account for castles as well.
-
-                print(f"****made move: {msg}")
-
-                # Status.write_game_status_to_disk(board)
-
-                # If we got here, we added a new move to the move queue, so we flip turn
-                return True
-
-            raise ValueError("We shouldn't reach this point in the function.")
-
-    if __name__ == '__main__':
-        print("No main for this file, please use `cliinterface.py`")
