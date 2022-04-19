41e10dfac53a7992978b569946a7328f7c755476
Nashir Janmohamed <nashirbass@gmail.com>
2022-04-19 01:49:09 -0400

    Fix pylint warnings for streamlit dashboard

--- chess-AI/streamlit_dashboard.py
+++ chess-AI/streamlit_dashboard.py
@@ -62,19 +62,17 @@ class Dashboard:
         self.col2.title("Get Model")
 
         # get directory for model and dataset
-        data_dir = self.col1.text_input("Dataset Directory","datasets/")
-        model_dir = self.col2.text_input("Model Directory","models/")
+        data_dir = self.col1.text_input("Dataset Directory", "datasets/")
+        model_dir = self.col2.text_input("Model Directory", "models/")
 
         # get the file names for each model / dataset
-        ds_file_name = self.col1.text_input("Dataset file name","dataset-06-04-22:19.pt")
-        m_file_name = self.col2.text_input("Model file name","model-06-04-22:19.pt")
+        ds_file_name = self.col1.text_input("Dataset file name", "dataset-06-04-22:19.pt")
+        m_file_name = self.col2.text_input("Model file name", "model-06-04-22:19.pt")
 
-        ds_load = self.col1.selectbox('Load Dataset',
-        ('None', 'Locally', 'Figshare'))
+        ds_load = self.col1.selectbox('Load Dataset', ('None', 'Locally', 'Figshare'))
 
-        m_load = self.col2.selectbox('Load Model',
-        ('None', 'Locally', 'Figshare'))
-   
+        m_load = self.col2.selectbox('Load Model', ('None', 'Locally', 'Figshare'))
+
         self.col1.title("Save Dataset")
         self.col2.title("Save Model")
 
@@ -83,36 +81,36 @@ class Dashboard:
         m_save_figshare = self.col2.checkbox("Save model to Figshare")
 
         # get MCTS save frequency
-        save_freq = self.col2.slider('MCTS Save freq',1,100)
+        save_freq = self.col2.slider('MCTS Save freq', 1, 100)
 
         # get figshare and local load flags
-        ds_figshare_load = True if ds_load == 'Figshare' else False
-        ds_local_load = True if ds_load == 'Local' else False
-        m_figshare_load = True if m_load == 'Figshare' else False
-        m_local_load = True if m_load == 'Local' else False
+        ds_figshare_load = (ds_load == 'Figshare')
+        ds_local_load = (ds_load == 'Local')
+        m_figshare_load = (m_load == 'Figshare')
+        m_local_load = (m_load == 'Local')
 
         ds_saving = {
             "data_dir": data_dir,
             "file_name": ds_file_name,
             "figshare_load": ds_figshare_load,
             "local_load": ds_local_load,
-            "figshare_save":ds_save_figshare
+            "figshare_save": ds_save_figshare
         }
 
         m_saving = {
             "model_dir": model_dir,
             "file_name": m_file_name,
-            "figshare_load":m_figshare_load,
+            "figshare_load": m_figshare_load,
             "local_load": m_local_load,
-            "figshare_save":m_save_figshare,
+            "figshare_save": m_save_figshare,
             "mcts_check_freq": save_freq
         }
-        
-        self.col1.subheader('Params')
-        self.col1.code(ds_saving,language = None)
-        self.col1.code(m_saving, language = None)
 
-        return ds_saving,m_saving
+        self.col1.subheader('Params')
+        self.col1.code(ds_saving, language=None)
+        self.col1.code(m_saving, language=None)
+
+        return ds_saving, m_saving
 
     def stockfish_params(self):
         """Sets all the necessary parameters for stockfish.
