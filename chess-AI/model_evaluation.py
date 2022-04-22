447edbb3b8e2ac4bc1ba9a692343df8368333477
JuddB <jbrockedgar@gmail.com>
2022-04-21 20:30:11 -0400

    model evaluation
    Still needs model loading, I will add soon but someone else will need to test since I have trouble loading a model on my laptop.

--- chess-AI/model_evaluation.py
+++ chess-AI/model_evaluation.py
@@ -4,29 +4,38 @@ These tests help us decide which models to keep during learning.
 """
 import chess
 import torch
+import random
 
-from mcts import MCTS
+from mcts import Mcts
+from ai_io import init_params
+from nn_layout import PlayNetwork
+from state_representation import get_cnn_input
+from output_representation import policy_converter
 from util import create_stockfish_wrapper
+
 
 
 class Knightr0Player:
     '''"AI" class that is a wrapper around our custom modification of AlphaZero.
     '''
-    def __init__(self, path_to_model):
-        self.model = torch.load(path_to_model)
+    def __init__(self, path_to_model=None):
+        if not(path_to_model == None):
+            self.model = torch.load(path_to_model)
+        else:
+            self.model = PlayNetwork(testing=True).to('cpu')
         # TODO: Update exploration rate.
-        self.mcts = MCTS(exploration_rate=0.01)
+        self.mcts = Mcts(exploration=0.01, device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'))
 
-    def select_move(self, board, fen):
+    def select_move(self, board):
         '''Sets stockfish state from provided fen and returns best move.
         '''
-        return mcts.get_best_move(fen, model)
+        return Mcts.get_best_move(board.fen(), self.model)
 
 
-class StockfishPlayer:
+class EasyStockfishPlayer:
     """"AI" class that is a simple wrapper around the Stockfish engine.
     """
-    def __init__(self, elo_rating=1300):
+    def __init__(self, elo_rating=1000):
         self.stockfish = create_stockfish_wrapper()
         self.stockfish.set_elo_rating(elo_rating)
 
@@ -34,32 +43,91 @@ class StockfishPlayer:
         """Sets stockfish state from provided fen and returns best move.
         """
         self.stockfish.set_fen_position(board.fen())
-        return self.stockfish.get_best_move()
+        return chess.Move.from_uci(self.stockfish.get_best_move())
 
     def __str__(self):
-        return "StockfishPlayer"
+        return "EasyStockfishPlayer"
 
-
-class RandomStockfishPlayer:
-    """"AI" class that is a simple wrapper around the Stockfish engine, generating random moves.
+class HardStockfishPlayer:
+    """"AI" class that is a simple wrapper around the Stockfish engine.
     """
-    def __init__(self, elo_rating=1300):
+    def __init__(self, elo_rating=1800):
         self.stockfish = create_stockfish_wrapper()
         self.stockfish.set_elo_rating(elo_rating)
 
     def select_move(self, board):
-        """Sets stockfish state from provided fen and returns random move.
+        """Sets stockfish state from provided fen and returns best move.
         """
         self.stockfish.set_fen_position(board.fen())
-        moves = self.stockfish.get_top_moves(len(list(board.legal_moves)))
-        return moves[random.randint(0, len(moves) - 1)]
+        return chess.Move.from_uci(self.stockfish.get_best_move())
+
+    def __str__(self):
+        return "HardStockfishPlayer"
+
+
+class RandomPlayer:
+    """"AI" class that is a simple wrapper around the Stockfish engine, generating random moves.
+    """
+    def select_move(self, board):
+        """Sets stockfish state from provided fen and returns random move.
+        """
+        # if type(move) is not str:
+        #     move = move['Move']
+        return random.choice(list(board.legal_moves))
 
     def __str__(self):
         return "RandomStockfishPlayer"
 
 
-def play_game(player1, player2):
-    pass
+def testModel(num_games=5):
+    wins = [0, 0, 0]
+    player = Knightr0Player()
+    enemyAI = [RandomPlayer(), EasyStockfishPlayer(), HardStockfishPlayer()]
+    for _ in range(num_games):
+        white = False
+        for i in range(len(enemyAI)):
+            white = not(white)
+            board = chess.Board()
+            if white:
+                while True:
+                    policy,_ = player.model(get_cnn_input(board))
+                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
+                    if board.is_game_over():
+                        print(board)
+                        print()
+                        if board.is_checkmate():
+                            wins[i] += 1 
+                        break
+                    
+                    board.push(enemyAI[i].select_move(board))
+                    if board.is_game_over():
+                        print(board)
+                        print()
+                        break
+                    
+            else:
+                while True:
+                    board.push(enemyAI[i].select_move(board))
+                    if board.is_game_over():
+                        print(board)
+                        print()
+                        break
+                    
+                    policy,_ = player.model(get_cnn_input(board))
+                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
+                    if board.is_game_over():
+                        print(board)
+                        print()
+                        if board.is_checkmate():
+                            wins[i] += 1
+                        break
+                
+    print(f"Num wins vs random: {wins[0]}/{num_games}")
+    print("Win rate vs random: " + str(wins[0] / num_games))
+    print(f"Num wins vs easy: {wins[1]}/{num_games}")
+    print("Win rate vs easy: " + str(wins[1] / num_games))
+    print(f"Num wins vs hard: {wins[2]}/{num_games}")
+    print("Win rate vs hard: " + str(wins[2] / num_games))
 
 
 def evaluate_two_players(player1, player2):
@@ -67,13 +135,8 @@ def evaluate_two_players(player1, player2):
 
 
 def main():
-    # play tournament between some number of AIs (new model, previous best model, random stockfish player, weak stockfish, strong stockfish) and save pgn of each game
-    # compute elo scores using BayesElo program and write ratings to a file
-    # return dict of elo scores for each 
+    testModel(50)
 
-    # to stop relative elo from changing over time, we normalize elo w.r.t. 1000 rated stockfish
-    # we want weak stockfish to always be rated 1000 and strong stockfish to be rated 2500
-
-
-
+if __name__ == "__main__":
+    main() 
 
