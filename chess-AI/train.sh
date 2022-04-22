829f435943a99aacd7c1cdbf3a75a66e2a99050f
Nashir Janmohamed <nashirbass@gmail.com>
2022-04-21 20:44:04 -0400

    Update script to use python 3

--- chess-AI/train.sh
+++ chess-AI/train.sh
@@ -8,7 +8,7 @@ if [ "$1" = "streamlit" ]; then
     streamlit run train.py -- -d
 
 elif [ "$1" = "json" ]; then
-    python train.py -j "${@:2}"
+    python3 train.py -j "${@:2}"
 else
     echo "Expected first argument to be either <streamlit> or <json>"
 fi
