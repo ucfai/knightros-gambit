cf847c13e469b4d03cf38d420f11f637dc9567e2
Nashir Janmohamed <nashirbass@gmail.com>
2022-04-21 20:29:52 -0400

    Update some style things

--- chess-AI/ai_io.py
+++ chess-AI/ai_io.py
@@ -5,6 +5,7 @@ import argparse
 from datetime import datetime
 import json
 import os
+
 import torch
 
 from figshare_api import FigshareApi
@@ -12,9 +13,8 @@ import options
 from streamlit_dashboard import Dashboard
 
 class DatasetSaving:
-    """
-    All the parameters for saving and loading datasets
-    
+    """Helper class containing the parameters used for saving and loading datasets.
+
     Attributes:
         data_dir: the directory where datasets will be saved and loaded from
         file_name: the file name of the dataset to load
@@ -22,8 +22,7 @@ class DatasetSaving:
         local_load: flag for if dataset should be loaded from local directory
         figshare_save: flag for if dataset should be saved to figshare
     """
-
-    def __init__(self,data_dir,file_name,figshare_load,local_load,figshare_save):
+    def __init__(self, data_dir, file_name, figshare_load, local_load, figshare_save):
         self.data_dir = data_dir
         self.file_name = file_name
         self.figshare_load = figshare_load
@@ -32,8 +31,7 @@ class DatasetSaving:
 
 
 class ModelSaving:
-    """
-    All the parameters for saving and loading models
+    """Helper class containing the parameters used for saving and loading models.
 
     Attributes:
         model_dir: the directory where models will be saved
@@ -44,16 +42,16 @@ class ModelSaving:
         mcts_check_freq: frequency for how often model should be saved
         checkpoint_path: the path where models should be saved during checkpointing
     """
-
-    def __init__(self,model_dir,file_name,figshare_load,local_load,figshare_save,mcts_check_freq):
+    def __init__(self, model_dir, file_name, figshare_load, local_load, figshare_save,
+        mcts_check_freq, checkpoint_path):
         self.model_dir = model_dir
         self.file_name = file_name
         self.figshare_load = figshare_load
         self.local_load = local_load
         self.figshare_save = figshare_save
         self.mcts_check_freq = mcts_check_freq
-        self.checkpoint_path = None
-        
+        self.checkpoint_path = checkpoint_path
+
 
 def file_from_path(path):
     """Splits up the full file path into the directory and file."""
@@ -111,7 +109,6 @@ def save_dataset(dataset, dataset_saving):
     will be saved. For saving to Figshare, the file still needs to
     be saved locally first.
     """
-
     date_string = create_date_string()
     # get the full file path to save
     full_path = dataset_saving.data_dir + "dataset-" +  date_string + ".pt"
@@ -131,13 +128,12 @@ def save_dataset(dataset, dataset_saving):
 def save_model(nnet, model_saving, checkpointing, file_name=None):
     """Save a model to Figshare or Locally.
 
-    model_saving specifies the paths, file names, and flags for loading and 
+    model_saving specifies the paths, file names, and flags for loading and
     saving nnet model weights. If figshare_save is true then the model
-    will be saved to figshare, otherwise model will be saved locally. 
+    will be saved to figshare, otherwise model will be saved locally.
     checkpoint_path specifies the path where the models from checkpointing will
     be saved. model_dir is the directory where models should be saved and loaded from.
     """
-
     if checkpointing:
         assert file_name is not None, "For checkpointing, a file name must be specified"
         full_path = model_saving.checkpoint_path + file_name + ".pt"
@@ -180,7 +176,7 @@ def load_model(nnet, model_saving, show_dash):
 
 
 def init_params(nnet, device):
-    '''Initialize parameters used for training.
+    """Initialize parameters used for training.
 
     Depending on value of flags passed to program, either set parameters from json file or from
     the streamlit dashboard (see streamlit_dashboard.py).
@@ -202,55 +198,54 @@ def init_params(nnet, device):
         start_train: Bool, True by default if reading from file, else, set by train button on the
             streamlit dashboard.
         show_dash: Bool, specifies whether or not to show the dashboard.
-    '''
-
+    """
     parser = argparse.ArgumentParser(
-        description='Specifies whether to run train.py with streamlit or json. Note: if --json is '
-                    'specified, it takes precedence over --dashboard.')
-    parser.add_argument('-j', '--json',
-                        dest='json',
-                        action='store_true',
-                        help='if specified, load params from file')
-    parser.add_argument('-d', '--dashboard',
-                        dest='dashboard',
-                        action='store_true',
-                        help='if specified, load params from dashboard')
-    parser.add_argument('-m', '--make_dataset',
-                        dest='make_dataset',
-                        action='store_true',
-                        help='if specified, a dataset will be created')
-    parser.add_argument('-s', '--disable_stockfish',
-                        dest='stockfish_train',
-                        action='store_false',
-                        help='if specified, stockfish train will be disabled')
-    parser.add_argument('-t', '--disable_mcts',
-                        dest='mcts_train',
-                        action='store_false',
-                        help='if specified, mcts train will be disabled')
+        description="Specifies whether to run train.py with streamlit or json. Note: if --json is "
+                    "specified, it takes precedence over --dashboard.")
+    parser.add_argument("-j", "--json",
+                        dest="json",
+                        action="store_true",
+                        help="if specified, load params from file")
+    parser.add_argument("-d", "--dashboard",
+                        dest="dashboard",
+                        action="store_true",
+                        help="if specified, load params from dashboard")
+    parser.add_argument("-m", "--make_dataset",
+                        dest="make_dataset",
+                        action="store_true",
+                        help="if specified, a dataset will be created")
+    parser.add_argument("-s", "--disable_stockfish",
+                        dest="stockfish_train",
+                        action="store_false",
+                        help="if specified, stockfish train will be disabled")
+    parser.add_argument("-t", "--disable_mcts",
+                        dest="mcts_train",
+                        action="store_false",
+                        help="if specified, mcts train will be disabled")
     args = parser.parse_args()
 
     if args.json:
-        with open('params.json') as file:
+        with open("params.json") as file:
             params = json.load(file)
 
-        model_saving_dict = params['saving']['model_saving']
-        dataset_saving_dict = params['saving']['dataset_saving']
+        model_saving_dict = params["saving"]["model_saving"]
+        dataset_saving_dict = params["saving"]["dataset_saving"]
 
-        learning_rate = params['misc_params']['lr']
-        momentum = params['misc_params']['momentum']
-        weight_decay = params['misc_params']['weight_decay']
+        learning_rate = params["misc_params"]["lr"]
+        momentum = params["misc_params"]["momentum"]
+        weight_decay = params["misc_params"]["weight_decay"]
 
-        stock_epochs = params['stockfish']['epochs']
-        stock_batch_size = params['stockfish']['batch_size']
-        stock_games = params['stockfish']['games']
-        elo, depth = params['stockfish']['elo'], params['stockfish']['depth']
+        stock_epochs = params["stockfish"]["epochs"]
+        stock_batch_size = params["stockfish"]["batch_size"]
+        stock_games = params["stockfish"]["games"]
+        elo, depth = params["stockfish"]["elo"], params["stockfish"]["depth"]
 
-        mcts_epochs = params['mcts']['epochs']
-        mcts_batch_size = params['mcts']['batch_size']
-        mcts_games = params['mcts']['games']
-        exploration = params['mcts']['exploration']
-        training_episodes = params['mcts']['training_episodes']
-        mcts_simulations = params['mcts']['simulations']
+        mcts_epochs = params["mcts"]["epochs"]
+        mcts_batch_size = params["mcts"]["batch_size"]
+        mcts_games = params["mcts"]["games"]
+        exploration = params["mcts"]["exploration"]
+        training_episodes = params["mcts"]["training_episodes"]
+        mcts_simulations = params["mcts"]["simulations"]
 
         make_dataset = args.make_dataset
         stockfish_train = args.stockfish_train
@@ -321,7 +316,7 @@ def init_params(nnet, device):
 
     # add checkpointing directory
     model_saving.checkpoint_path = model_saving.model_dir + "checkpoint-" + \
-                                    create_date_string() + '/'
+                                    create_date_string() + "/"
     make_dir(model_saving.checkpoint_path)
 
     stockfish_options = options.StockfishOptions(learning_rate, momentum, weight_decay,
