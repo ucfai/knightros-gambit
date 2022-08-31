64dcbd6d62c70eac053a9ef6c943cdcc489d02d9
JuddB <jbrockedgar@gmail.com>
2022-08-30 22:09:01 -0400

    get elo
    Plays certain number of games between every combination of given chess AI then gets a normalized relative elo for each. Currently uses our AI, an AI that plays randomly, an 800 rated stockfish AI, a 1800 rated stockfish AI, and gets a relative elo between them.

--- chess-AI/model_evaluation.py
+++ chess-AI/model_evaluation.py
@@ -5,6 +5,8 @@ These tests help us decide which models to keep during learning.
 import chess
 import torch
 import random
+import itertools
+import numpy as np
 
 from mcts import Mcts
 from ai_io import init_params
@@ -12,7 +14,6 @@ from nn_layout import PlayNetwork
 from state_representation import get_cnn_input
 from output_representation import policy_converter
 from util import create_stockfish_wrapper
-
 
 
 class Knightr0Player:
@@ -31,13 +32,17 @@ class Knightr0Player:
         '''
         return Mcts.get_best_move(board.fen(), self.model)
 
+    def __str__(self):
+        return "Knightr0Player"
+
 
 class EasyStockfishPlayer:
     """"AI" class that is a simple wrapper around the Stockfish engine.
     """
-    def __init__(self, elo_rating=1000):
+    def __init__(self, elo_rating=800):
         self.stockfish = create_stockfish_wrapper()
         self.stockfish.set_elo_rating(elo_rating)
+        self.stockfish.depth = 5
 
     def select_move(self, board):
         """Sets stockfish state from provided fen and returns best move.
@@ -48,12 +53,16 @@ class EasyStockfishPlayer:
     def __str__(self):
         return "EasyStockfishPlayer"
 
+
 class HardStockfishPlayer:
     """"AI" class that is a simple wrapper around the Stockfish engine.
     """
+    
     def __init__(self, elo_rating=1800):
         self.stockfish = create_stockfish_wrapper()
         self.stockfish.set_elo_rating(elo_rating)
+        self.stockfish.depth = 9
+        self.elo = elo_rating
 
     def select_move(self, board):
         """Sets stockfish state from provided fen and returns best move.
@@ -77,66 +86,100 @@ class RandomPlayer:
 
     def __str__(self):
         return "RandomStockfishPlayer"
+        
 
-
-def testModel(num_games=5):
-    wins = [0, 0, 0]
-    player = Knightr0Player()
-    enemyAI = [RandomPlayer(), EasyStockfishPlayer(), HardStockfishPlayer()]
+def evaluate_two_players(player1, player2, num_games=20):
+    white = False
+    pointsP1 = 0
     for _ in range(num_games):
-        white = False
-        for i in range(len(enemyAI)):
-            white = not(white)
-            board = chess.Board()
-            if white:
-                while True:
-                    policy,_ = player.model(get_cnn_input(board))
+        white = not(white)
+        board = chess.Board()
+        if white:
+            while True:
+                if isinstance(player1, Knightr0Player):
+                    policy,_ = player1.model(get_cnn_input(board))
                     board.push_uci(policy_converter.find_best_legal_move(policy, board))
-                    if board.is_game_over():
-                        print(board)
-                        print()
-                        if board.is_checkmate():
-                            wins[i] += 1 
-                        break
-                    
-                    board.push(enemyAI[i].select_move(board))
-                    if board.is_game_over():
-                        print(board)
-                        print()
-                        break
-                    
-            else:
-                while True:
-                    board.push(enemyAI[i].select_move(board))
-                    if board.is_game_over():
-                        print(board)
-                        print()
-                        break
-                    
-                    policy,_ = player.model(get_cnn_input(board))
-                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
-                    if board.is_game_over():
-                        print(board)
-                        print()
-                        if board.is_checkmate():
-                            wins[i] += 1
-                        break
+                else:
+                    board.push(player1.select_move(board))
+                if board.is_game_over(claim_draw=True):
+                    if board.is_checkmate():
+                        pointsP1 += 0.5
+                    pointsP1 += 0.5
+                    break
                 
-    print(f"Num wins vs random: {wins[0]}/{num_games}")
-    print("Win rate vs random: " + str(wins[0] / num_games))
-    print(f"Num wins vs easy: {wins[1]}/{num_games}")
-    print("Win rate vs easy: " + str(wins[1] / num_games))
-    print(f"Num wins vs hard: {wins[2]}/{num_games}")
-    print("Win rate vs hard: " + str(wins[2] / num_games))
+                if isinstance(player2, Knightr0Player):
+                    policy,_ = player2.model(get_cnn_input(board))
+                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
+                else:
+                    board.push(player2.select_move(board))
+                if board.is_game_over(claim_draw=True):
+                    if not(board.is_checkmate()):
+                        pointsP1 += 0.5
+                    break        
+        else:
+            while True:
+                if isinstance(player2, Knightr0Player):
+                    policy,_ = player2.model(get_cnn_input(board))
+                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
+                else:
+                    board.push(player2.select_move(board))
+                if board.is_game_over(claim_draw=True):
+                    if not(board.is_checkmate()):
+                        pointsP1 += 0.5
+                    break
+                    
+                if isinstance(player1, Knightr0Player):
+                    policy,_ = player1.model(get_cnn_input(board))
+                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
+                else:
+                    board.push(player1.select_move(board))
+                if board.is_game_over(claim_draw=True):
+                    if board.is_checkmate():
+                        pointsP1 += 0.5
+                    pointsP1 += 0.5
+                    break
+    pointsP2 = num_games - pointsP1
+    return pointsP1, pointsP2
 
-
-def evaluate_two_players(player1, player2):
-    board = chess.Board()
-
+def get_elo(points, num_games):
+    expected = 0.5
+    k_value = 32
+    elos = np.zeros_like(points)
+    length = len(points)
+    for i in range(length):
+        elos[i] = k_value * (points[i] - expected * num_games)
+        
+    maximum = max(elos)
+    minimum = min(elos)
+    for i in range(length):
+        elos[i] = int(((elos[i] - minimum) / (maximum - minimum)) * (HardStockfishPlayer().elo))
+    return elos
 
 def main():
-    testModel(50)
+    num_games = 30
+    players = [Knightr0Player(), RandomPlayer(), EasyStockfishPlayer(), HardStockfishPlayer()]
+    tot_pts = np.zeros_like(players)
+    matches = list(itertools.combinations(players, 2))
+    pts = np.zeros_like(matches)
+    length = len(matches)
+    for i in range(length):
+        pts[i][0], pts[i][1] = evaluate_two_players(matches[i][0], matches[i][1], num_games)
+        for j in range(2):
+            if isinstance(matches[i][j], Knightr0Player):
+                tot_pts[0] += pts[i][j]
+            elif isinstance(matches[i][j], RandomPlayer):
+                tot_pts[1] += pts[i][j]
+            elif isinstance(matches[i][j], EasyStockfishPlayer):
+                tot_pts[2] += pts[i][j]
+            elif isinstance(matches[i][j], HardStockfishPlayer):
+                tot_pts[3] += pts[i][j]  
+
+    elos = get_elo(tot_pts, num_games)
+    print("\nRelative Elo")
+    print("Knightr0:\t" + str(elos[0]))
+    print("Random:\t\t" + str(elos[1]))
+    print("Easy Stockfish:\t" + str(elos[2]))
+    print("Hard Stockfish:\t" + str(elos[3]))
 
 if __name__ == "__main__":
     main() 
-
