60206cd51be62b84d98cea263ce60d7bd465cf63
JuddB <jbrockedgar@gmail.com>
2022-09-08 19:23:10 -0400

    Update model_evaluation.py
    replaces repetitive code with function make_move(). There is a bit more I could replace with a function but I found it looked gross bc I had to replace the break statements with a boolean variable and had to include conditionals to check if the game was not over after every turn.

--- chess-AI/model_evaluation.py
+++ chess-AI/model_evaluation.py
@@ -58,7 +58,7 @@ class HardStockfishPlayer:
     """"AI" class that is a simple wrapper around the Stockfish engine.
     """
     
-    def __init__(self, elo_rating=1800):
+    def __init__(self, elo_rating=2000):
         self.stockfish = create_stockfish_wrapper()
         self.stockfish.set_elo_rating(elo_rating)
         self.stockfish.depth = 9
@@ -96,43 +96,27 @@ def evaluate_two_players(player1, player2, num_games=20):
         board = chess.Board()
         if white:
             while True:
-                if isinstance(player1, Knightr0Player):
-                    policy,_ = player1.model(get_cnn_input(board))
-                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
-                else:
-                    board.push(player1.select_move(board))
+                board = make_move(player1, board)
                 if board.is_game_over(claim_draw=True):
                     if board.is_checkmate():
                         pointsP1 += 0.5
                     pointsP1 += 0.5
                     break
                 
-                if isinstance(player2, Knightr0Player):
-                    policy,_ = player2.model(get_cnn_input(board))
-                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
-                else:
-                    board.push(player2.select_move(board))
+                board = make_move(player2, board)
                 if board.is_game_over(claim_draw=True):
                     if not(board.is_checkmate()):
                         pointsP1 += 0.5
                     break        
         else:
             while True:
-                if isinstance(player2, Knightr0Player):
-                    policy,_ = player2.model(get_cnn_input(board))
-                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
-                else:
-                    board.push(player2.select_move(board))
+                board = make_move(player2, board)
                 if board.is_game_over(claim_draw=True):
                     if not(board.is_checkmate()):
                         pointsP1 += 0.5
                     break
                     
-                if isinstance(player1, Knightr0Player):
-                    policy,_ = player1.model(get_cnn_input(board))
-                    board.push_uci(policy_converter.find_best_legal_move(policy, board))
-                else:
-                    board.push(player1.select_move(board))
+                board = make_move(player1, board)
                 if board.is_game_over(claim_draw=True):
                     if board.is_checkmate():
                         pointsP1 += 0.5
@@ -140,6 +124,14 @@ def evaluate_two_players(player1, player2, num_games=20):
                     break
     pointsP2 = num_games - pointsP1
     return pointsP1, pointsP2
+
+def make_move(player, board):
+    if isinstance(player, Knightr0Player):
+        policy,_ = player.model(get_cnn_input(board))
+        board.push_uci(policy_converter.find_best_legal_move(policy, board))
+    else:
+        board.push(player.select_move(board))
+    return board
 
 def get_elo(points, num_games):
     expected = 0.5
@@ -156,7 +148,7 @@ def get_elo(points, num_games):
     return elos
 
 def main():
-    num_games = 30
+    num_games = 20
     players = [Knightr0Player(), RandomPlayer(), EasyStockfishPlayer(), HardStockfishPlayer()]
     tot_pts = np.zeros_like(players)
     matches = list(itertools.combinations(players, 2))
